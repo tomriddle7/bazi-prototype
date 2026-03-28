059e286f2d913d6a7d1c1d73363697176a1af95e
tomriddle7 <tomriddle7@naver.com>
2026-03-28 09:16:09 +0900

    Init Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# bazi-prototype
-
--- src/providers/main.ts
+++
@@ -1,9 +0,0 @@
-import ApiService from './services/api.service';
-import SajuService from './services/saju.service';
-import SajuLocalService from './services/saju-local.service';
-
-export {
-    ApiService,
-    SajuService,
-    SajuLocalService,
-};
